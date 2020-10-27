ee48ee03dcbfa3b761be6198b814c3924f4df1cd
Leonardo Elis <71516548+leonardoelis@users.noreply.github.com>
2020-10-27 09:50:49 -0300

    Página para gerar o relatório

---
+++ paginaRelatorio_resp.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+    
+    if (empty($_SESSION['ra']) and  empty($_SESSION['senha'])) {
+        header('Location: login.php');
+    }
+    
+    require_once 'banco.php';
+    $banco = new Banco();
+    
+    $bloco = filter_input(INPUT_POST, 'bloco');
+    
+    $tabela = $banco->gerarRelatorio($bloco);
+ 
+    
+    
+    
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Pesquisa CPA</title>
+        <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/excellentexport@3.4.3/dist/excellentexport.min.js"></script>
+    </head>
+    <body>
+        <?php
+            echo $tabela;
+        ?>
+        
+        <br>
+        <br>
+        <a download="relatorio.xls" href="#" onclick="return ExcellentExport.excel(this, 'Relatorio', 'Relatório');">Export to Excel</a>
+        <br>
+        <a download="relatorio.csv" href="#" onclick="return ExcellentExport.csv(this, 'Relatorio');">Export to CSV</a>
+    </body>
+</html>
+
