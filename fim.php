6d38ef66a94efd1d4988c426bc8c43c079015109
bruccioli <64874091+bruccioli@users.noreply.github.com>
2020-11-13 15:46:35 -0300

    Update fim.php

--- fim.php
+++ fim.php
@@ -6,12 +6,29 @@ and open the template in the editor.
 -->
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>Pesquisa CPA</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" media="(max-width: 640px)" href="max-640px.css">
+        <link rel="stylesheet" media="(min-width: 640px)" href="min-640px.css">
+        <link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/estilo3.css" />
+        <script src="lib/jquery/jquery.min.js"></script>
+        <script src="lib/bootstrap/js/bootstrap.min.js"></script>
     </head>
-    <body>
-        <div>Fim da pesquisa</div>
+    <body background="img/fundoPagina.png">
+        <header>
+            <div>
+                <img class="logo" src="img/CPA_Logo_UAM.png" />
+            </div>
+        </header>
+        
+        <div class="fim">
+            
+            <h2 class="fim">Fim</h2>
+            <h2 class="fim">Agradecemos a participação!</h2>
+            
+        </div>
     </body>
 </html>
 <?php
