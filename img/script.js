c89ee4ffe074926023068637c61e4b7891d6d314
guilherme-cararetti <72578213+guilherme-cararetti@users.noreply.github.com>
2020-11-16 18:35:46 -0300

    Delete script.js

--- img/script.js
+++
@@ -1,7 +0,0 @@
-var array_imagens = [imagem1.png, imagem2.jpg, imagem3.png, imagem4.jpg];
-
-function displayImage()
-{
-    var num = Math.floor(Math.random() * (array_imagens.length+1));
-    document.canvas.src = array_imagens[num];
-}
